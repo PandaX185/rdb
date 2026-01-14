62aaa99997d9eef939495bbf34af73c975c45c2a
PandaX185 <mouss3abbb@gmail.com>
2026-01-15 01:32:09 +0200

    Refactor TCP server to use epoll for efficient client handling and manage read/write buffers

--- src/net/tcp_server.cpp
+++ src/net/tcp_server.cpp
@@ -4,57 +4,29 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <thread>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <map>
+#include <string>
 
 namespace net
 {
-    void write_all(int fd, const std::string &data)
+    struct ClientState
     {
-        size_t total = 0;
-        while (total < data.size())
-        {
-            ssize_t n = write(fd, data.data() + total, data.size() - total);
-            if (n <= 0)
-                break;
-            total += n;
-        }
-    }
+        std::string read_buffer;
+        std::string write_buffer;
+    };
 
-    void handle_client(int client_fd, RequestHandler handler)
+    void set_nonblock(int fd)
     {
-        char buffer[4096];
-        std::string full_buffer;
-        while (true)
-        {
-            ssize_t n = read(client_fd, buffer, sizeof(buffer));
-            if (n <= 0)
-                break;
-
-            full_buffer.append(buffer, n);
-
-            while (true)
-            {
-                auto pos = full_buffer.find("\n");
-                if (pos == std::string::npos)
-                    break;
-
-                std::string line = full_buffer.substr(0, pos);
-                full_buffer.erase(0, pos + 1);
-
-                if (!line.empty() && line.back() == '\r')
-                    line.pop_back();
-
-                std::string response = handler(line);
-                write_all(client_fd, response);
-            }
-        }
-
-        close(client_fd);
+        int flags = fcntl(fd, F_GETFL, 0);
+        fcntl(fd, F_SETFL, flags | O_NONBLOCK);
     }
 
     void TCPServer::start()
     {
         int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+        set_nonblock(server_fd);
 
         sockaddr_in addr{};
         addr.sin_family = AF_INET;
@@ -66,12 +38,95 @@ namespace net
 
         std::cout << "Server started on port " << port << std::endl;
 
+        int epfd = epoll_create1(0);
+        struct epoll_event ev;
+        ev.events = EPOLLIN;
+        ev.data.fd = server_fd;
+        epoll_ctl(epfd, EPOLL_CTL_ADD, server_fd, &ev);
+
+        std::map<int, ClientState> clients;
+        const int MAX_EVENTS = 64;
+        struct epoll_event events[MAX_EVENTS];
+
         while (true)
         {
-            int client_fd = accept(server_fd, nullptr, nullptr);
-            if (client_fd >= 0)
+            int n = epoll_wait(epfd, events, MAX_EVENTS, -1);
+            for (int i = 0; i < n; ++i)
             {
-                std::thread(handle_client, client_fd, handler).detach();
+                int fd = events[i].data.fd;
+                if (fd == server_fd)
+                {
+                    int client_fd;
+                    while ((client_fd = accept(server_fd, nullptr, nullptr)) >= 0)
+                    {
+                        set_nonblock(client_fd);
+                        ev.events = EPOLLIN;
+                        ev.data.fd = client_fd;
+                        epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &ev);
+                        clients[client_fd] = ClientState{};
+                    }
+                }
+                else
+                {
+                    auto &state = clients[fd];
+                    if (events[i].events & EPOLLIN)
+                    {
+                        char buf[4096];
+                        ssize_t nread = read(fd, buf, sizeof(buf));
+                        if (nread > 0)
+                        {
+                            state.read_buffer.append(buf, nread);
+                            size_t pos;
+                            while ((pos = state.read_buffer.find('\n')) != std::string::npos)
+                            {
+                                std::string line = state.read_buffer.substr(0, pos);
+                                state.read_buffer.erase(0, pos + 1);
+                                if (!line.empty() && line.back() == '\r')
+                                    line.pop_back();
+                                std::string response = handler(line);
+                                state.write_buffer += response;
+                            }
+                            if (!state.write_buffer.empty())
+                            {
+                                ev.events = EPOLLIN | EPOLLOUT;
+                                ev.data.fd = fd;
+                                epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev);
+                            }
+                        }
+                        else if (nread == 0)
+                        {
+                            epoll_ctl(epfd, EPOLL_CTL_DEL, fd, nullptr);
+                            close(fd);
+                            clients.erase(fd);
+                        }
+                        else
+                        {
+                            epoll_ctl(epfd, EPOLL_CTL_DEL, fd, nullptr);
+                            close(fd);
+                            clients.erase(fd);
+                        }
+                    }
+                    if (events[i].events & EPOLLOUT)
+                    {
+                        ssize_t nwrite = write(fd, state.write_buffer.data(), state.write_buffer.size());
+                        if (nwrite > 0)
+                        {
+                            state.write_buffer.erase(0, nwrite);
+                            if (state.write_buffer.empty())
+                            {
+                                ev.events = EPOLLIN;
+                                ev.data.fd = fd;
+                                epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev);
+                            }
+                        }
+                        else
+                        {
+                            epoll_ctl(epfd, EPOLL_CTL_DEL, fd, nullptr);
+                            close(fd);
+                            clients.erase(fd);
+                        }
+                    }
+                }
             }
         }
     }
